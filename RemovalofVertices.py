0271f471138e2e0d17511d4f831cbb90179483d3
snktmishra2 <snktmishra2@gmail.com>
2020-04-22 17:48:42 +0530

    50 points

---
+++ RemovalofVertices.py
@@ -0,0 +1,33 @@
+t=int(input())
+for i in range(t):
+    n,k=map(int,input().split())
+    l=[]
+    for i in range(1,n+1):
+        l.append(i)
+        
+    pos=((k*2)+2)%n;
+    s=n
+    if (pos==0):
+        pos=n
+    l.pop(pos-1)
+    s=s-1
+    pos=pos-1
+    while(s!=1):
+        #s!=1
+    
+       pos=pos+2
+       if(pos>s):
+           #s
+           pos=pos%(s)
+                   #s
+          
+       l.pop(pos-1)
+       s=s-1
+       if(s==1):
+           #s
+           print(l[0])
+           break
+       pos=pos-1
+        
+
+			
