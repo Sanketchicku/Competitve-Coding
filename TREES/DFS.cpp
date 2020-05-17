f43ea738c604f03b17e207d1e7ef4804ae6fa864
sanket <snktmishra2@gmail.com>
2020-05-17 16:51:19 +0530

    Depth First Search

---
+++ TREES/DFS.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+void dfs(vector<ll>adj[],bool vis[],ll node) 
+{
+	vis[node]={true};
+	cout<<node<<" ";
+	
+	for(auto i:adj[node])
+	{
+		//cout<<i<<" ";
+		
+		if(vis[i]==false)
+		{
+		dfs(adj,vis,i);
+		//cout<<"called";
+		}
+	}
+	
+}
+	
+	
+int main()
+{
+	//n-number of vertices
+	//m-number of edges
+	ll n,m;
+	cin>>n>>m;
+	vector<ll>adj[n+1];
+	
+	for(int i=0;i<m;i++)
+	{
+		int u,v;
+		cin>>u>>v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	bool vis[n+1]={false};
+	dfs(adj,vis,1);
+return 0;
+}
+/*6 8 
+1 2
+1 3
+2 4
+2 5
+3 5
+4 5
+4 6
+5 6*/
