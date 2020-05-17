c2035e262affbda00d0a1c06cbb751fa765199b7
sanket <snktmishra2@gmail.com>
2020-05-17 16:54:27 +0530

    Depth First Search

---
+++ GRAPHS/BFS.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+void bfs(vector<ll>adj[],ll n) 
+{
+	queue<ll>q;
+	q.push(3);
+	bool vis[n+1]={false};
+	vis[3]=true;
+	
+	while(!q.empty())
+	{
+		int node=q.front();
+		q.pop();
+		cout<<node<<" ";
+		
+		for(auto it:adj[node])
+		{
+			if(!vis[it])
+			{
+				vis[it]=true;
+				q.push(it);
+			}
+		}
+	}
+}
+	
+	
+int main()
+{
+	//n-number of vertices
+	//m-number of edges
+	ll n,m;
+	cin>>n>>m;
+	vector<ll>adj[n+1];
+	
+	for(int i=0;i<m;i++)
+	{
+		int u,v;
+		cin>>u>>v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	bfs(adj,n);
+}
+
+/*6 8 
+1 2
+1 3
+2 4
+2 5
+3 5
+4 5
+4 6
+5 6*/
